22aefb40df3c63ea9e6db767e077229406414920
Marin Bailhe <43707993+biadzok@users.noreply.github.com>
2022-11-07 16:02:08 +0000

    dernier exo

---
+++ heure.java
@@ -0,0 +1,11 @@
+public class Heure {
+  private int heures;
+  private int minutes;
+  private int secondes;
+
+  public Heure(int heures, int minutes, int secondes) {
+    this.heures = heures;
+    this.minutes = minutes;
+    this.secondes = secondes;
+  }
+}
